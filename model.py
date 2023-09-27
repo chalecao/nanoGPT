e5b94ea0782dc0c4247d2b45cd5e6f29f06be899
haomou.chh <haomou.chh@alibaba-inc.com>
2023-09-27 09:24:51 +0800

    增加中文解释

--- model.py
+++ model.py
@@ -15,14 +15,16 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+# 这段代码定义了一个名为LayerNorm的类，继承自nn.Module。该类是一个对PyTorch的LayerNorm进行了简单封装的自定义层。
 class LayerNorm(nn.Module):
     """ LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False """
 
+    # 在初始化方法__init__中，该类接受两个参数：ndim（表示输入张量的维度）和bias（一个布尔值，表示是否使用偏置项）。在初始化过程中，该类创建了两个可学习的参数：self.weight和self.bias。其中self.weight是一个形状为(ndim,)的可训练参数，初始值为全1；self.bias是一个形状为(ndim,)的可训练参数，如果bias为True，则初始化为全0，如果bias为False，则为None。
     def __init__(self, ndim, bias):
         super().__init__()
         self.weight = nn.Parameter(torch.ones(ndim))
         self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
-
+        # 在前向传播方法forward中，该类调用了PyTorch的F.layer_norm函数来实现标准化操作。F.layer_norm函数接受多个参数：input（输入张量），shape（表示layer norm应该应用在哪些维度上），weight（表示应用于每个输入维度的权重，默认为None），bias（表示应用于每个输入维度的偏置项，默认为None），eps（一个小的浮点数，用于数值稳定性，默认为1e-5）。
     def forward(self, input):
         return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
 
@@ -30,30 +32,39 @@ class CausalSelfAttention(nn.Module):
 
     def __init__(self, config):
         super().__init__()
+        # 这是一个断言语句，用于确保config.n_embd（embedding维度）可以被config.n_head（头数）整除。如果不满足条件，将会抛出AssertionError。
         assert config.n_embd % config.n_head == 0
+        # 这是一个线性层，用于进行键（key）、查询（query）和值（value）的投影。输入维度为config.n_embd，输出维度为3 * config.n_embd。是否使用偏置项由config.bias决定。
         # key, query, value projections for all heads, but in a batch
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+        # 这是另一个线性层，用于进行最终的输出投影。输入维度和输出维度都是config.n_embd，是否使用偏置项由config.bias决定。
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
         # regularization
+        # 这是两个Dropout层，用于进行正则化。参数config.dropout控制Dropout层的丢弃概率。
         self.attn_dropout = nn.Dropout(config.dropout)
         self.resid_dropout = nn.Dropout(config.dropout)
+        # 这是两个保存配置信息的实例变量，分别表示头数（n_head）和embedding维度（n_embd）。
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.dropout = config.dropout
         # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
+        # 这段代码判断是否支持Flash Attention。如果PyTorch版本大于等于2.0，即torch.nn.functional中存在scaled_dot_product_attention函数，则支持Flash Attention。如果不支持，会打印警告信息。
         self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
         if not self.flash:
             print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
             # causal mask to ensure that attention is only applied to the left in the input sequence
+            # 如果不支持Flash Attention，则执行以下代码：self.register_buffer：这是一个用于注册缓冲区的方法，用于存储不需要梯度更新的数据。这里注册了一个名为"bias"的缓冲区，存储了一个下三角矩阵，用于在自注意力机制中生成注意力掩码。下三角矩阵的形状为(config.block_size, config.block_size)，并且通过.view方法将其形状变为(1, 1, config.block_size, config.block_size)。
             self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
                                         .view(1, 1, config.block_size, config.block_size))
 
     def forward(self, x):
+        # 获取输入张量x的形状信息，其中B表示批量大小（batch size），T表示序列长度（sequence length），C表示嵌入维度（embedding dimensionality）。
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
-
+        # 通过调用self.c_attn线性层，将输入张量x进行投影，得到键（key）、查询（query）和值（value）的张量。然后使用split方法在维度2上将投影结果分割为self.n_embd个部分，即将维度2的大小分割为self.n_embd份。
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
         q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
+        # 将键、查询和值的张量进行形状变换。将维度C分割为self.n_head份，并将维度B和T与self.n_head进行转置，使得self.n_head成为新的批量维度。
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
@@ -61,58 +72,92 @@ class CausalSelfAttention(nn.Module):
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         if self.flash:
             # efficient attention using Flash Attention CUDA kernels
+            # 使用Flash Attention的CUDA内核进行高效的注意力计算，得到输出张量y。scaled_dot_product_attention函数接受键、查询和值的张量，attn_mask掩码（默认为None），dropout_p丢弃概率（如果在训练模式下使用self.dropout，否则为0），is_causal是否是因果注意力（即只考虑左侧的输入序列）。
             y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
         else:
             # manual implementation of attention
+            # 通过手动实现的注意力计算方式，计算注意力得分。通过矩阵乘法计算查询和键的点积，然后除以sqrt(C / n_head)进行缩放。
             att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+            # 根据注意力掩码，将无效位置的注意力得分置为负无穷。注意力掩码是通过注册的缓冲区"bias"来实现的，只有左下三角部分为1，其余部分为0。
             att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+            # 对注意力得分进行softmax归一化，得到注意力权重。
             att = F.softmax(att, dim=-1)
+            # 对注意力权重进行dropout操作。
             att = self.attn_dropout(att)
+            # 通过矩阵乘法将注意力权重与值相乘，得到输出张量y。
             y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        # 将所有头部的输出张量重新组合在一起，通过转置、连续化和形状变换操作，得到最终的输出张量y。
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
 
         # output projection
+        # 对输出张量y进行残差连接和正则化操作。首先将y通过self.c_proj线性层进行投影，然后再进行残差连接，最后通过self.resid_dropout进行dropout操作。
         y = self.resid_dropout(self.c_proj(y))
+        # 返回输出张量y作为前向传播的结果。
         return y
-
+    
+# 这段代码定义了一个MLP（多层感知机）类，继承自nn.Module。MLP是一个包含多个全连接层和激活函数的神经网络模型。
 class MLP(nn.Module):
 
     def __init__(self, config):
         super().__init__()
+        # 这是一个线性层，用于进行全连接操作。输入维度为config.n_embd，输出维度为4 * config.n_embd。是否使用偏置项由config.bias决定。
         self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+        # 这是一个GELU（Gaussian Error Linear Units）激活函数层。GELU激活函数是一种非线性函数，具有平滑的S形特征。
         self.gelu    = nn.GELU()
+        # 这是另一个线性层，用于进行最终的输出投影。输入维度为4 * config.n_embd，输出维度为config.n_embd。是否使用偏置项由config.bias决定。
         self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        # 这是一个Dropout层，用于进行正则化。参数config.dropout控制Dropout层的丢弃概率。
         self.dropout = nn.Dropout(config.dropout)
-
+    # forward方法中，给定输入张量x，按照以下顺序进行处理：
     def forward(self, x):
+        # 使用self.c_fc线性层进行全连接操作。
         x = self.c_fc(x)
+        # 通过GELU激活函数进行非线性变换。
         x = self.gelu(x)
+        # 使用self.c_proj线性层进行最终的输出投影。
         x = self.c_proj(x)
+        # 应用Dropout层进行正则化操作。
         x = self.dropout(x)
+        # 返回处理后的张量x作为前向传播的结果。
         return x
 
+# 这段代码定义了一个Block类，继承自nn.Module。Block是一个Transformer中的基本组件，包含了自注意力机制和MLP。
 class Block(nn.Module):
 
     def __init__(self, config):
         super().__init__()
+        # 这是一个LayerNorm层，用于进行层归一化。输入维度为config.n_embd，是否使用偏置项由config.bias决定。
         self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
+        # 这是一个CausalSelfAttention类的实例，用于进行自注意力机制的处理。注意力机制根据输入张量x计算查询（query）、键（key）和值（value），并生成注意力权重。
         self.attn = CausalSelfAttention(config)
+        # 这是另一个LayerNorm层，用于进行层归一化。输入维度为config.n_embd，是否使用偏置项由config.bias决定。
         self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
+        # 这是一个MLP类的实例，用于进行多层感知机的处理。MLP包含了全连接层和激活函数，用于非线性变换。
         self.mlp = MLP(config)
-
+    # forward方法中，给定输入张量x
     def forward(self, x):
+        # 将输入张量x先经过层归一化self.ln_1，然后通过自注意力机制self.attn处理，并将处理结果与输入张量相加。这实现了残差连接（residual connection）。
         x = x + self.attn(self.ln_1(x))
+        # 将上一步得到的张量x再经过层归一化self.ln_2，然后通过多层感知机self.mlp处理，并将处理结果与前一步的张量相加。同样实现了残差连接。
         x = x + self.mlp(self.ln_2(x))
         return x
 
+# 这段代码定义了一个数据类（dataclass）GPTConfig，用于保存GPT模型的配置参数。通过定义这个数据类，可以方便地存储和访问GPT模型的配置参数。可以根据需要修改这些配置参数，以满足不同的实验需求。
 @dataclass
 class GPTConfig:
-    block_size: int = 1024
+    # 整数，表示模型输入的最大序列长度，默认为1024。
+    block_size: int = 1024 
+    # 整数，表示词汇表大小，默认为50304。这里的词汇表大小是为了适配GPT-2模型，而且会被填充到最接近64的倍数，以提高计算效率。
     vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    # 整数，表示Transformer中的层数，默认为12。
     n_layer: int = 12
+    # 整数，表示自注意力机制中的头数，默认为12。
     n_head: int = 12
+    # 整数，表示嵌入维度，默认为768。
     n_embd: int = 768
+    # 浮点数，表示Dropout层的丢弃概率，默认为0.0（不使用Dropout）。
     dropout: float = 0.0
+    # 布尔值，表示是否在线性层和层归一化中使用偏置项。如果为True，则使用偏置项，类似于GPT-2模型。如果为False，则不使用偏置项，会稍微好一些，并且更快。
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
 class GPT(nn.Module):
